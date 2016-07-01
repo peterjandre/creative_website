bfea30575cc8155f4745d877475fddba42ee248f
Peter Andre <peterjandre@gmail.com>
2016-07-01 08:27:35 -0400

    revised dir structure

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp');
+var sass = require('gulp-ruby-sass');
+var bs = require('browser-sync').create();
+
+gulp.task('build-css', function() {
+  return sass('styles/style.scss')
+  .on('error', sass.logError)
+  .pipe(gulp.dest('styles/style.css'))
+})
+
+gulp.task('bs-start', function() {
+  bs.init({
+    server: {
+      baseDir: "./"
+    }
+  })
+})
+
+gulp.task('default', function() {
+  console.log("I gulped it down")
+})
